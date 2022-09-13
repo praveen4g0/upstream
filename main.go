5593a324918ab5f1ec3399999665cc630e25f710
praveen4g0 <praveen.thangadancha@gmail.com>
2022-09-13 11:34:03 +0530

    Add seed value to randomize numbers properly
    Signed-off-by: praveen4g0 <praveen.thangadancha@gmail.com>

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"math/big"
 	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	fmt.Printf("Here is a random %T ) : %d\n", big.NewInt(int64(rand.Int())), big.NewInt(int64(rand.Int())))
 }
